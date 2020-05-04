76eee36bf9c2c7010c387ee62a8ec25d6ef79c9e
cola119 <k.cola119@ezweb.ne.jp>
2020-05-04 12:48:46 +0900

    getUserMedia and play stream

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export const App: React.FC<{}> = () => {
-  return <div>healalo</div>;
+  const myVideoRef = useRef<HTMLVideoElement | null>(null);
+  const peerVideoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (!myVideoRef.current) return;
+    const init = async (): Promise<void> => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      console.log(stream, myVideoRef.current);
+      myVideoRef.current!.srcObject = stream;
+    };
+    init();
+  }, [myVideoRef.current]);
+
+  return (
+    <div>
+      <div>healalo</div>
+      <video
+        autoPlay
+        controls
+        muted
+        ref={myVideoRef}
+        style={{ width: '300px', border: '1px solid #000000' }}
+      />
+      <video
+        autoPlay
+        controls
+        muted
+        ref={peerVideoRef}
+        style={{ width: '300px', border: '1px solid #000000' }}
+      />
+    </div>
+  );
 };
