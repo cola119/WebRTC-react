61f56bb51892c388c7d616b7a421d08873569cfb
cola119 <k.cola119@ezweb.ne.jp>
2020-05-04 12:21:18 +0900

    react hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const WorkerPlugin = require('worker-plugin');
+
 module.exports = {
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: {
-          loader: "ts-loader",
+          loader: 'ts-loader',
           options: {
             transpileOnly: true,
           },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx", ".json", "mjs", ".wasm"],
+    extensions: ['.js', '.ts', '.tsx', '.json', 'mjs', '.wasm'],
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+    }),
+    new WorkerPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    inline: true,
+    hot: true,
   },
 };
