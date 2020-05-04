3f8f5f65dd9fd8114b1871f4ed8fb643db0a5b0a
cola119 <k.cola119@ezweb.ne.jp>
2020-05-04 11:39:51 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".json", "mjs", ".wasm"],
+  },
+};
